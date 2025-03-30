1d97511b08be6427315c5e53aaf30da3e95cd02f
Saskia Tellenbach <Saskia_tellenbac@sluz.ch>
2025-03-30 17:13:02 +0200

    4.3 Beheben des Sonderfalls durch 0 dividieren.

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -53,15 +53,12 @@ test("divide eight through two is four", () => {
   expect(actual).toBe(expected);
 });
 
-test("divide eight through zero is zero", () => {
+
+test("divide eight through zero throws an error", () => {
   // Arrange
   const a: number = 8;
   const b: number = 0;
-  const expected: number = 0;
 
-  // Act
-  const actual: number = divide(a, b);
-
-  // Assert
-  expect(actual).toBe(expected);
+  // Act & Assert
+  expect(() => divide(a, b)).toThrow("Division durch 0 ist nicht erlaubt");
 });
--- src/calculator.ts
+++ src/calculator.ts
@@ -11,6 +11,8 @@ export function multiplied(faktorA: number, faktorB: number): number {
 }
 
 export function divide(dividient: number, divisor: number): number {
+  if (divisor === 0) {
+    throw new Error("Division durch 0 ist nicht erlaubt");
+  }
   return dividient / divisor;
 }
-
